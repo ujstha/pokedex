5c68d837e5ec265f64f91eac8a796770dcdaca3f
ujstha <ujjawalshrestha7@gmail.com>
2024-06-12 18:46:08 +0300

    (feat-components) - Loader component; assets added; tailwind configured

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import Loader from "./components/Loader.vue"
+</script>
 
 <template>
   <h1>Template HelloWorld</h1>
+  <Loader />
 </template>
 
 <style scoped></style>
---
+++ src/components/Loader.vue
@@ -0,0 +1,24 @@
+<template>
+  <img
+    src="../assets/pokeball-icon.png"
+    class="brightness-90 loading-ball"
+    height="60"
+    width="60"
+    alt="Pokeball Loader"
+  />
+</template>
+
+<style scoped>
+.loading-ball {
+  animation: rotatePokeBall ease-in-out 3s infinite;
+}
+
+@keyframes rotatePokeBall {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
